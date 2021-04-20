7142f29e13e2fca75537ea16c857fab37ad8db86
dwibagus154 <dwibagus154@gmail.com>
2021-04-21 00:01:23 +0700

    tambah main program

--- jadwalTutor.py
+++ jadwalTutor.py
@@ -54,26 +54,23 @@ def getID2(data, data2) :
 def message(title, text):
    tk.MessageBox.showinfo(title, text)
 
-def saveForm():
-    # conn = sqlite3.connect('Tutorin.db')
-    # c = conn.cursor()
-    
+def saveForm(textbox1, textbox3, varje, varting, varmapel, vardur, varhari, varjam, textbox4): 
     conn = mysql.connect(host="localhost", user="root", password="", database="tutorin")
     c = conn.cursor()
 
-    id = int(textbox1.get())
-    nama = textbox3.get()
+    id = textbox1
+    nama = textbox3
     jen = ""
-    if(int(varje.get()) == 1) :
+    if(int(varje) == 1) :
         jen = "SMP"
-    elif(int(varje.get()) == 2) :
+    elif(int(varje) == 2) :
         jen = "SMA"
-    ting = varting.get()
-    mapel = varmapel.get()
-    durasi = vardur.get()
-    hari = varhari.get()
-    jam = getIntJam(varjam.get())
-    desc = textbox4.get()
+    ting = varting
+    mapel = varmapel
+    durasi = vardur
+    hari = varhari
+    jam = getIntJam(varjam)
+    desc = textbox4
     if(desc == ""):
         desc = "Tidak Ada"
     #id, nama, jenjang, tingkat, mapel, durasi, hari, jam, des
@@ -91,11 +88,8 @@ def saveForm():
         nrow = [id,courseid,hari,jam,durasi,desc]
         print(nrow)
         #insert data to database
-        # c.execute("INSERT INTO JadwalTutor VALUES (id,courseid,'"+courseid+"'")")
-        # print("(",id,",",courseid,",'" +hari +"',",jam,",",durasi,",'"+ desc +"')")
-        c.execute("insert into jadwaltutor(tutorid, courseid, hari, jamMulai, durasi, deskripsi) values(",id,",",courseid,",'"+hari+"',",jam,",",durasi,",'"+desc+"')")
-        
-        #show messagebox
+        c.execute("insert into jadwaltutor(tutorid, courseid, hari, jamMulai, durasi, deskripsi) values(",id,","+ str(courseid)+",'"+hari+"',"+str(jam)+","+ str(durasi)+",'"+desc+"')")
+         #show messagebox
         messagebox.showinfo("Info", "Berhasil Menyimpan Data")
         #print("sukses")
     conn.commit()
@@ -106,128 +100,27 @@ def raise_frame(fname):
     
 
 
-HEIGHT = 670
-WIDTH = 800
+# HEIGHT = 670
+# WIDTH = 800
 
-jadwal = tk.Tk()
+# jadwal = tk.Tk()
 
-#create canvas
-canvas = tk.Canvas(jadwal, height = HEIGHT, width = WIDTH)
-canvas.pack()
+# #create canvas
+# canvas = tk.Canvas(jadwal, height = HEIGHT, width = WIDTH)
+# canvas.pack()
 
-
-#create frame
-frame = tk.Frame(jadwal, bg = '#80c1ff', bd = 2)
-frame.place(relwidth=0.9, relheight=0.9, relx=0.05, rely=0.05)
-
-frame2 = tk.Frame(jadwal, bg = '#80c1ff', bd = 2) #ini buat lihat calender
-frame2.place(relwidth=0.9, relheight=0.9, relx=0.05, rely=0.05)
-
-
-#==============frame1===================
-label1 = tk.Label(frame, text="Form Jadwal Tutor", bg = '#80c1ff', font=50)
-label1.place(relx=0.4, rely=0)
-
-#username/ID
-label2 = tk.Label(frame, text='ID', font=40, bg = '#80c1ff')
-label2.place(relx=0.1, rely=0.1)
-
-textbox1 = tk.Entry(frame, font=40)
-textbox1.place(relx=0.3, rely=0.1, relwidth=0.6)
-
-#nama
-label3 = tk.Label(frame, text='Nama', font=40, bg = '#80c1ff')
-label3.place(relx=0.1, rely=0.15)
-
-textbox3 = tk.Entry(frame, font=40)
-textbox3.place(relx=0.3, rely=0.15, relwidth=0.6)
-
-#jenjang pendidikan mapel
-label4 = tk.Label(frame, text='Jenjang ', font=40, bg = '#80c1ff')
-label4.place(relx=0.1, rely=0.20)
-
-varje = tk.IntVar()
-jeng1 = tk.Radiobutton(frame, text="SMP", variable=varje, value=1)
-jeng1.place(relx=0.3, rely=0.2)
-
-jeng2 = tk.Radiobutton(frame, text="SMA", variable=varje, value=2)
-jeng2.place(relx=0.5, rely=0.2)
-
-#tingkat
-label4 = tk.Label(frame, text='Tingkat', font=40, bg = '#80c1ff')
-label4.place(relx=0.1, rely=0.25)
-
-varting = tk.IntVar()
-ting1 = tk.Radiobutton(frame, text="Kelas 1", variable=varting, value=1)
-ting1.place(relx=0.3, rely=0.25)
-
-ting2 = tk.Radiobutton(frame, text="Kelas 2", variable=varting, value=2)
-ting2.place(relx=0.5, rely=0.25)
-
-ting3 = tk.Radiobutton(frame, text="Kelas 3", variable=varting, value=3)
-ting3.place(relx=0.7, rely=0.25)
-
-#matapelajaran
-label4 = tk.Label(frame, text='Mata Pelajaran', font=40, bg = '#80c1ff')
-label4.place(relx=0.1, rely=0.3)
-
-mapelAva = ('Biologi', 'Matematika', 'Fisika', 'Kimia', 'Ekonomi', 'Sosiologi', 'Geografi', 
-            'Bahasa Inggris', 'Bahasa Indonesia', 'Sejarah')
-varmapel = tk.StringVar()
-mapelEntry = Combobox(frame, value=mapelAva, textvariable = varmapel)
-mapelEntry.place(relx=0.3, rely=0.3, relwidth=0.6)
-
-#durasi
-label4 = tk.Label(frame, text='Durasi', font=40, bg = '#80c1ff')
-label4.place(relx=0.1, rely=0.35)
-
-vardur = tk.IntVar()
-dur1 = tk.Radiobutton(frame, text="1 Jam", variable=vardur, value=1)
-dur1.place(relx=0.3, rely=0.35)
-
-dur2 = tk.Radiobutton(frame, text="2 Jam", variable=vardur, value=2)
-dur2.place(relx=0.5, rely=0.35)
-
-#hari
-label4 = tk.Label(frame, text='Hari ', font=40, bg = '#80c1ff')
-label4.place(relx=0.1, rely=0.4)
-
-hariAva = ('Senin', 'Selasa', 'Rabu', 'Kamis', 'Jumat', 'Sabtu', 'Minggu')
-varhari = tk.StringVar()
-hariEntry = Combobox(frame, value=hariAva, textvariable = varhari)
-hariEntry.place(relx=0.3, rely=0.4, relwidth=0.6)
-              
-
-#jam
-label4 = tk.Label(frame, text='Jam Mulai', font=40, bg = '#80c1ff')
-label4.place(relx=0.1, rely=0.45)
-
-varjam = tk.StringVar()
-jamAva = ('08.00 WIB', '09.00 WIB','10.00 WIB','11.00 WIB','12.00 WIB','13.00 WIB','14.00 WIB','15.00 WIB',
-            '16.00 WIB','17.00 WIB','18.00 WIB','19.00 WIB','20.00 WIB')
-jamEntry = Combobox(frame, value=jamAva, textvariable = varjam)
-jamEntry.place(relx=0.3, rely=0.45, relwidth=0.6)
-
-#deskripsi
-label4 = tk.Label(frame, text='Deskripsi', font=40, bg = '#80c1ff')
-label4.place(relx=0.1, rely=0.5)
-
-textbox4 = tk.Entry(frame, font=25)
-textbox4.place(relx=0.3, rely=0.5, relwidth=0.6, relheight=0.25)
-
-button = tk.Button(frame, text="Submit Form", bd=2, command=saveForm)
-button.place(relx=0.75, rely=0.8)
-
-button2 = tk.Button(frame, text="Back", bd=2, command=lambda:raise_frame(frame2))
-button2.place(relx=0.65, rely=0.8)
 
 #======frame2=====
-def showSchedule(list) :
+def showSchedule(list, textf1) :
     conn = sqlite3.connect('Tutorin.db')
     c = conn.cursor()
+    # conn = mysql.connect(host="localhost", user="root", password="", database="tutorin")
+    # c = conn.cursor()
+    
 
     #tampilkan schedule dari tutor
-    id = textboxf1.get()
+    print(textf1)
+    id = textf1
 
     #ambil data jadwal
     c.execute("SELECT rowid, * FROM JadwalTutor WHERE tutorID = (?)", (id,))
@@ -282,35 +175,3 @@ def deleteJadwal(list):
         conn.commit()
         conn.close()
 
-
-labelf1 = tk.Label(frame2, text="Jadwal Tutor", bg = '#80c1ff', font=50)
-labelf1.place(relx=0.4, rely=0)
-
-labelf2 = tk.Label(frame2, text='My ID', font=40, bg = '#80c1ff')
-labelf2.place(relx=0.1, rely=0.1)
-
-textboxf1 = tk.Entry(frame2, font=25)
-textboxf1.place(relx=0.2, rely=0.1, relwidth=0.5)
-
-scrollbar = tk.Scrollbar(frame2, orient=tk.VERTICAL)
-scrollbar.place(relx=0.05, rely=0.2, relwidth=0.9, relheight=0.7)
-
-mylist = tk.Listbox(frame2, yscrollcommand = scrollbar.set, font=40 )
-mylist.place(relx=0.05, rely=0.2, relwidth=0.9, relheight=0.7)
-
-scrollbar.config( command = mylist.yview )
-
-buttonf1 = tk.Button(frame2, text="Submit", bd=2, command=lambda:showSchedule(mylist))
-buttonf1.place(relx=0.75, rely=0.1)
-
-buttonf2 = tk.Button(frame2, text="Add Schedule", bd=2, command=lambda:raise_frame(frame))
-buttonf2.place(relx=0.75, rely=0.93)
-
-buttonf3 = tk.Button(frame2, text="Delete Selected Schedule", bd=2, command=lambda:deleteJadwal(mylist))
-buttonf3.place(relx=0.5, rely=0.93)
-
-
-
-
-raise_frame(frame2)
-jadwal.mainloop()
--- searchTutor.py
+++ searchTutor.py
@@ -4,18 +4,18 @@ from tkinter.ttk import Combobox
 from tkinter import messagebox
 import sqlite3
 
-def searchTutor(list):
+def searchTutor(list, varje, varting, varmapel, varhari):
    conn = sqlite3.connect('Tutorin.db')
    c = conn.cursor()
    list.delete(0,list.size())
    jen = ""
-   if(int(varje.get()) == 1) :
+   if(int(varje) == 1) :
       jen = "SMP"
-   elif(int(varje.get()) == 2) :
+   elif(int(varje) == 2) :
       jen = "SMA"
-   ting = varting.get() #default = 0
-   mapel = varmapel.get()
-   hari = str(varhari.get())
+   ting = varting #default = 0
+   mapel = varmapel
+   hari = str(varhari)
    # print(hari)
    # print(mapel)
    # print(ting)
@@ -82,85 +82,85 @@ def searchTutor(list):
 def konfirmasiPemesanan():
    MsgBox = messagebox.askquestion("askquestion", "Apakah Ada Yakin Ingin Melakukan Pemesanan?")
 
-HEIGHT = 670
-WIDTH = 800
+# HEIGHT = 670
+# WIDTH = 800
 
-search = tk.Tk()
+# search = tk.Tk()
 
-#create canvas
-canvas = tk.Canvas(search, height = HEIGHT, width = WIDTH)
-canvas.pack()
+# #create canvas
+# canvas = tk.Canvas(search, height = HEIGHT, width = WIDTH)
+# canvas.pack()
 
-#create frame
-frame = tk.Frame(canvas, bg = '#80c1ff', bd = 2)
-frame.place(relwidth=0.9, relheight=0.3, relx=0.05, rely=0.05)
+# #create frame
+# frame = tk.Frame(canvas, bg = '#80c1ff', bd = 2)
+# frame.place(relwidth=0.9, relheight=0.3, relx=0.05, rely=0.05)
 
-lower_frame = tk.Frame(search, bg = '#80c1ff', bd = 2)
-lower_frame.place(relwidth=0.9, relheight=0.6, relx=0.05, rely=0.35)
+# lower_frame = tk.Frame(search, bg = '#80c1ff', bd = 2)
+# lower_frame.place(relwidth=0.9, relheight=0.6, relx=0.05, rely=0.35)
 
-#judul
-label1 = tk.Label(frame, text="Search Tutor", bg = '#80c1ff', font=40)
-label1.place(relx=0.4, rely=0)
+# #judul
+# label1 = tk.Label(frame, text="Search Tutor", bg = '#80c1ff', font=40)
+# label1.place(relx=0.4, rely=0)
 
-#matapelajaran
-label4 = tk.Label(frame, text='Mata Pelajaran', font=40, bg = '#80c1ff')
-label4.place(relx=0.1, rely=0.2)
+# #matapelajaran
+# label4 = tk.Label(frame, text='Mata Pelajaran', font=40, bg = '#80c1ff')
+# label4.place(relx=0.1, rely=0.2)
 
-mapelAva = ('Biologi', 'Matematika', 'Fisika', 'Kimia', 'Ekonomi', 'Sosiologi', 'Geografi', 
-            'Bahasa Inggris', 'Bahasa Indonesia', 'Sejarah')
-varmapel = tk.StringVar()
-mapelEntry = Combobox(frame, value=mapelAva, textvariable = varmapel, state="readonly")
-mapelEntry.place(relx=0.3, rely=0.2, relwidth=0.6)
+# mapelAva = ('Biologi', 'Matematika', 'Fisika', 'Kimia', 'Ekonomi', 'Sosiologi', 'Geografi', 
+#             'Bahasa Inggris', 'Bahasa Indonesia', 'Sejarah')
+# varmapel = tk.StringVar()
+# mapelEntry = Combobox(frame, value=mapelAva, textvariable = varmapel, state="readonly")
+# mapelEntry.place(relx=0.3, rely=0.2, relwidth=0.6)
 
-#jenjang pendidikan mapel
-label2 = tk.Label(frame, text='Jenjang ', font=40, bg = '#80c1ff')
-label2.place(relx=0.1, rely=0.35)
+# #jenjang pendidikan mapel
+# label2 = tk.Label(frame, text='Jenjang ', font=40, bg = '#80c1ff')
+# label2.place(relx=0.1, rely=0.35)
 
-varje = tk.IntVar()
-jeng1 = tk.Radiobutton(frame, text="SMP", variable=varje, value=1)
-jeng1.place(relx=0.3, rely=0.35)
+# varje = tk.IntVar()
+# jeng1 = tk.Radiobutton(frame, text="SMP", variable=varje, value=1)
+# jeng1.place(relx=0.3, rely=0.35)
 
-jeng2 = tk.Radiobutton(frame, text="SMA", variable=varje, value=2)
-jeng2.place(relx=0.5, rely=0.35)
+# jeng2 = tk.Radiobutton(frame, text="SMA", variable=varje, value=2)
+# jeng2.place(relx=0.5, rely=0.35)
 
-#tingkat
-label3 = tk.Label(frame, text='Tingkat', font=40, bg = '#80c1ff')
-label3.place(relx=0.1, rely=0.5)
+# #tingkat
+# label3 = tk.Label(frame, text='Tingkat', font=40, bg = '#80c1ff')
+# label3.place(relx=0.1, rely=0.5)
 
-varting = tk.IntVar()
-ting1 = tk.Radiobutton(frame, text="Kelas 1", variable=varting, value=1)
-ting1.place(relx=0.3, rely=0.5)
+# varting = tk.IntVar()
+# ting1 = tk.Radiobutton(frame, text="Kelas 1", variable=varting, value=1)
+# ting1.place(relx=0.3, rely=0.5)
 
-ting2 = tk.Radiobutton(frame, text="Kelas 2", variable=varting, value=2)
-ting2.place(relx=0.5, rely=0.5)
+# ting2 = tk.Radiobutton(frame, text="Kelas 2", variable=varting, value=2)
+# ting2.place(relx=0.5, rely=0.5)
 
-ting3 = tk.Radiobutton(frame, text="Kelas 3", variable=varting, value=3)
-ting3.place(relx=0.7, rely=0.5)
-
-
-#hari
-label5 = tk.Label(frame, text='Hari ', font=40, bg = '#80c1ff')
-label5.place(relx=0.1, rely=0.65)
-
-hariAva = ('Senin', 'Selasa', 'Rabu', 'Kamis', 'Jumat', 'Sabtu', 'Minggu')
-varhari = tk.StringVar()
-hariEntry = Combobox(frame, value=hariAva, textvariable = varhari, state="readonly")
-hariEntry.place(relx=0.3, rely=0.65, relwidth=0.6)
+# ting3 = tk.Radiobutton(frame, text="Kelas 3", variable=varting, value=3)
+# ting3.place(relx=0.7, rely=0.5)
 
 
-#lower
-scrollbar = tk.Scrollbar(lower_frame, orient=tk.VERTICAL)
-scrollbar.place(relx=0.05, rely=0, relwidth=0.9, relheight=0.85)
+# #hari
+# label5 = tk.Label(frame, text='Hari ', font=40, bg = '#80c1ff')
+# label5.place(relx=0.1, rely=0.65)
 
-mylist = tk.Listbox(lower_frame, yscrollcommand = scrollbar.set )
-mylist.place(relx=0.05, rely=0, relwidth=0.9, relheight=0.85)
+# hariAva = ('Senin', 'Selasa', 'Rabu', 'Kamis', 'Jumat', 'Sabtu', 'Minggu')
+# varhari = tk.StringVar()
+# hariEntry = Combobox(frame, value=hariAva, textvariable = varhari, state="readonly")
+# hariEntry.place(relx=0.3, rely=0.65, relwidth=0.6)
 
-scrollbar.config( command = mylist.yview )
 
-button = tk.Button(frame, text="Search", bd=2, command=lambda:searchTutor(mylist))
-button.place(relx=0.75, rely=0.8)
+# #lower
+# scrollbar = tk.Scrollbar(lower_frame, orient=tk.VERTICAL)
+# scrollbar.place(relx=0.05, rely=0, relwidth=0.9, relheight=0.85)
 
-button2 = tk.Button(lower_frame, text="Pesan Tutor", bd=2, command=konfirmasiPemesanan)
-button2.place(relx=0.75, rely=0.9)
+# mylist = tk.Listbox(lower_frame, yscrollcommand = scrollbar.set )
+# mylist.place(relx=0.05, rely=0, relwidth=0.9, relheight=0.85)
 
-search.mainloop()
+# scrollbar.config( command = mylist.yview )
+
+# button = tk.Button(frame, text="Search", bd=2, command=lambda:searchTutor(mylist))
+# button.place(relx=0.75, rely=0.8)
+
+# button2 = tk.Button(lower_frame, text="Pesan Tutor", bd=2, command=konfirmasiPemesanan)
+# button2.place(relx=0.75, rely=0.9)
+
+# search.mainloop()
