76c5ec56a373b5a2eee1eb5ae34cabd41bad3adf
slarkdarr <daffaresyaly@gmail.com>
2021-04-21 22:59:30 +0700

    Fix pylint bug

--- kehadiran.py
+++
@@ -1,7 +0,0 @@
-def tampilan_button():
-    return Button(root, text="click here", command=my_click,
-                  fg="black", bg="skyblue").pack()
-
-
-def my_click():
-    my_label = Label(root, text="hello").pack()
