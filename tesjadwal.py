8c4fb8e6fe5c7a9c09cae8bf6722995c52ddd601
nabilahann <13519097@std.stei.itb.ac.id>
2021-04-21 13:53:46 +0700

    fix jadwal lagii

--- tesjadwal.py
+++ tesjadwal.py
@@ -1,11 +1,9 @@
 from jadwalTutor2 import *
 
-e = jadwalTutor()
-
 #test getIntJam
-assert(e.getIntJam("08.00 WIB")==8)
-assert(e.getIntJam("10.00 WIB")==10)
-assert(e.getIntJam("00.00 WIB")==0)
+assert(getIntJam("08.00 WIB")==8)
+assert(getIntJam("10.00 WIB")==10)
+assert(getIntJam("00.00 WIB")==0)
 
 data = [(2, 'Biologi', 'SMA', 2),(3, 'Biologi', 'SMA', 3),(4, 'Biologi', 'SMP', 1),(5, 'Biologi', 'SMP', 2) ,
         (6, 'Biologi', 'SMP', 3),(7, 'Matematika', 'SMA', 1),(8, 'Matematika', 'SMA', 2),(9, 'Matematika', 'SMA', 3),(10, 'Matematika', 'SMP', 1),
@@ -13,15 +11,15 @@ data = [(2, 'Biologi', 'SMA', 2),(3, 'Biologi', 'SMA', 3),(4, 'Biologi', 'SMP', 
         (16, 'Fisika', 'SMP', 1),(17, 'Fisika', 'SMP', 2),(18, 'Fisika', 'SMP', 3)]
 
 #test getID
-assert(e.getID(data,'Biologi',2,'SMA')==2)
-assert(e.getID(data,'Matematika',2,'SMA')==8)
-assert(e.getID(data,'Fisika',2,'SMP')==17)
+assert(getID(data,'Biologi',2,'SMA')==2)
+assert(getID(data,'Matematika',2,'SMA')==8)
+assert(getID(data,'Fisika',2,'SMP')==17)
 
 #test getID2
 data2 = [(16, 'Fisika', 'SMP', 1)]
 data3 = [(7, 'Matematika', 'SMA', 1)]
-assert(e.getID2(data,data2)==16)
-assert(e.getID2(data,data3)==7)
+assert(getID2(data,data2)==16)
+assert(getID2(data,data3)==7)
 
 #test showSchedule
 
