6e26ff35d3b699e11b428586fb67d9000119e0e2
slarkdarr <daffaresyaly@gmail.com>
2021-04-21 22:29:54 +0700

    Commit to develop with gitignore

---
+++ dateTime.py
@@ -0,0 +1,26 @@
+class dateTime:
+	def __init__(self, year=0000, month=00, day=00, hour=00, minute=00, second=00):
+		self.year = year
+		self.month = month
+		self.day = day
+		self.hour = hour
+		self.minute = minute
+		self.second = second
+
+	def getYear():
+		return self.year
+
+	def getMonth():
+		return self.month
+
+	def getDay():
+		return self.day
+
+	def getHour():
+		return self.hour
+
+	def getMinute():
+		return self.minute
+
+	def getSecond():
+		return self.second
