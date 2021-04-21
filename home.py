cbb2e71ef60359f85a8deb84412ba03abad2344b
darubagus <darubagus@gmail.com>
2021-04-21 13:52:44 +0700

    benerin home

--- home.py
+++ home.py
@@ -11,27 +11,23 @@ class MainWindow:
         self.label.place(x=95, y=40)
         self.login = Button(self.app, text="Login", pady=5, padx=30, command=login)
         self.login.place(x=100, y=100)
-        self.registerT = Button(self.app, text="Register as Murid", pady=5, padx=20, command=registerM)
+        self.registerT = Button(self.app, text="Register as Murid", pady=5, padx=20, command=register_m)
         self.registerT.place(x=70, y=150)
-        self.registerM = Button(self.app, text="Register as Tutor", pady=5, padx=20, command=registerT)
+        self.registerM = Button(self.app, text="Register as Tutor", pady=5, padx=20, command=register_t)
         self.registerM.place(x=70, y=200)
 
     def run(self):
         self.app.mainloop()
-        
 def login():
     loginTk = Login()
     loginTk.run()
-
-def registerT():
+def register_t():
     registerTk = RegisterTutor()
     registerTk.run()
-
-def registerM():
+def register_m():
     registerTk = RegisterMurid()
     registerTk.run()
     # app = MainWindow()
     # app.run()
-
 app = MainWindow()
 app.run()
