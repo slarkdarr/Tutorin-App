65f1cefc15ee2a7a836d41628e5f7faa08e97b66
darubagus <darubagus@gmail.com>
2021-04-21 14:26:27 +0700

    add testcase

--- test_signinup.py
+++ test_signinup.py
@@ -3,5 +3,11 @@ from loginModule import *
 import mysql.connector as mysql
 
 
-def test_passVerification():
-    assert(validate('hajidadang', 'admin') == 1)
+def test_passVerification1():
+    assert(validate('hajidadang', 'admin') == 1) # validation success
+
+def test_passVerification2():
+    assert(validate('budianakbaik', 'helofren123') == 0) # wrong password
+
+def test_passVerification2():
+    assert(validate('rplasik', 'yaudahiya') == 0) # data not in database
